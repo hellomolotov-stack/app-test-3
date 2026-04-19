34d72a9d24fa4caa6b86bcc8b8f08fb439e4d64a
hellomolotov-stack <hellomolotov@gmail.com>
2026-04-19 13:07:07 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,21 @@
+// Конфигурация приложения
+export const GUEST_API_URL = 'https://script.google.com/macros/s/AKfycby0943sdi-neS00sFzcyT-rsmzQgPOD4vsOYMnnLYSK8XcEIQJynP1CGsSWP62gK1zxSw/exec';
+export const REGISTRATION_API_URL = 'https://script.google.com/macros/s/AKfycbxbtauKP7FO0quR0yktXfbnU-x_Vk6zOzKZlms-tgQSszVDQH1POGrREYdjPBzHqyUJFg/exec';
+export const ROBOKASSA_LINK = 'https://auth.robokassa.ru/merchant/Invoice/1PA1-yY5CEO9FPrxJnvIJw';
+export const SEASON_CARD_LINK = 'https://auth.robokassa.ru/merchant/Invoice/l8qjTjiBi06GlZIPFgo4Ug';
+export const PERMANENT_CARD_LINK = 'https://auth.robokassa.ru/merchant/Invoice/Es0zC2xYmkaM9Q-TvYgw0A';
+
+export const CACHE_TTL = 600000;
+export const DATA_TIMEOUT = 10000;
+
+// Firebase конфиг
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCv4v2CJxR1A-QkYWYjzFEF-kKWB1qUSQY",
+    authDomain: "hiking-club-app-b6c7c.firebaseapp.com",
+    databaseURL: "https://hiking-club-app-b6c7c-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "hiking-club-app-b6c7c",
+    storageBucket: "hiking-club-app-b6c7c.firebasestorage.app",
+    messagingSenderId: "507288460496",
+    appId: "1:507288460496:web:5a3381866b95e9096492d5",
+    measurementId: "G-2PBBHYD8JG"
+};
